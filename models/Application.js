5109280d721fbd49fb034f3150f119a08af3dcf9
Tracy-miranja <barazatracy44@gmail.com>
2024-10-22 10:11:58 +0300

    modify endpoints

--- models/Application.js
+++ models/Application.js
@@ -1,33 +1,69 @@
 const mongoose = require("mongoose");
 
-const applicationSchema = new mongoose.Schema({
+const applicationSchema = new mongoose.Schema(
+  {
     applicant: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      
     },
     job: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Job",
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Job",
+      
     },
     resume: {
-        type: String,
-        required: true,
+      type: String,
+      
     },
     coverLetter: {
-        type: String,
+      type: String,
     },
     status: {
-        type: String,
-        enum: ["applied", "interview", "hired", "rejected"],
-        default: "applied",
+      type: String,
+      enum: ["applied", "interview", "hired", "rejected"],
+      default: "applied",
     },
-    createdAt: {
-        type: Date,
-        default: Date.now,
+    // Personal Details
+    firstName: { type: String, required: true },
+    secondName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    idNumber: { type: String, required: true },
+    whatsAppNo: { type: String },  // Fixed typo
+    phoneNumber: { 
+      type: String, 
+      required: true
+      
     },
-});
+    email: { type: String, required: true },
+    age: { type: Number, required: true },
+    nationality: { type: String, required: true },
+    location: { type: String, required: true },
+    specialization: { type: String },
+    academicLevel: {
+      type: String,
+      enum: [
+        "Master's Degree",
+        "Postgraduate Diploma",
+        "Bachelor’s Degree",
+        "Associate's Degree",
+        "Diploma",
+        "Certificate",
+        "others",
+      ],
+    },
+    workExperience: [
+      {
+        company: { type: String },
+        position: { type: String },
+        duration: { type: String },
+      },
+    ],
+    salaryInfo: { type: String },
+    cv: { type: String }
+  },
+  { timestamps: true }  
+);
 
 const Application = mongoose.model("Application", applicationSchema);
 
